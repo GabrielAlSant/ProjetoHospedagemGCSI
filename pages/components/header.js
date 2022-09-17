10d334db442f0799214a6a9ef0978d1002a90483
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-17 18:39:04 -0300

    Base header

---
+++ pages/components/header.js
@@ -0,0 +1,43 @@
+export default function Header(){
+return (
+
+    <div>
+    <div class="container text-center">
+    <div class="row">
+    <div class="col">
+      <div className="title white">Livraria</div>
+    </div>
+    <div class="col">
+    </div>
+    <div class="col">
+    <nav className="navbar navbar-expand-lg bg">
+         <div>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" href="#"><span className="headitem white">Login</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"><span className="headitem white">Produtos</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"><span className="headitem white">Sacola</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"><span className="headitem white">Usuário</span></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+    </div>
+    </div>
+
+
+    </div>
+)
+}
