0e92a430b03911dc1ee099e8178133bc8a3ab098
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-18 16:05:25 -0300

    body

--- pages/components/body.js
+++ pages/components/body.js
@@ -1,5 +1,17 @@
 export default function Body(){
     return(
-        <div></div>
+        <div className="container">
+        <div className="imgtext">
+        <div className="row">
+          <div className="col">
+          <div className="card-body">
+          <div className="black">Às vezes a leitura é um modo engenhoso de evitar o pensamento.</div>
+          </div>
+          </div>
+          <div className="col">
+          </div>
+        </div>
+      </div>
+      </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ export default function Home() {
     <div className="background">
     <Header />
     </div>
-    <Body />
+    <div className="imgbody">
+     <Body />
+     </div>
     </div>
   )
 }
