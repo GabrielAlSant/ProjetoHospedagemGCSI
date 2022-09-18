ecf1522c1d9981a403916d78519a13d431b65d46
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-18 16:48:05 -0300

    headercard

--- pages/components/headercard.js
+++ pages/components/headercard.js
@@ -1,5 +1,12 @@
 export default function HeaderCard(){
     return (
-        <div></div>
+        <div className="displayheadercard">
+            <div className="start">
+            <div className="textheadercard">Itens na pré-venda</div>
+            </div>
+            <div className="end">
+            <button type="button" class="btn btn-dark">Veja mais</button>
+            </div>
+        </div>
     )
 }
