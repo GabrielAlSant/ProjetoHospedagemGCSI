d58d54adcc43b7ce9fa3dac0b3b5fcca49a1f728
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-18 14:53:46 -0300

    body

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from './components/header'
+import Body from './components/body'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
     <div className="background">
     <Header />
     </div>
+    <Body />
     </div>
   )
 }
