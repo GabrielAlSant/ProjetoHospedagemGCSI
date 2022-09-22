4710fd714e3118023ab93b00bdd07e0acf0377b9
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-22 06:36:30 -0400

    footer config

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -2,11 +2,11 @@ export default function Footer(){
     return(
         <div className="footer">
           <div className="row">
-            <div className=" col logo mt-3">
+            <div className=" col logo mt-4">
 Livraria
             </div>
-            <div className="col names mt-3">
-Gabriel Taína 
+            <div className="col names mt-4">
+Criadores: Gabriel && Taína 
             </div>
          </div>  
         </div>
