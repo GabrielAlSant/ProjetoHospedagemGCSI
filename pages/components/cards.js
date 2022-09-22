c05e84e9fc42edb10b072000b3806d4a3e8c033f
tainafernanda02 <taina.souza2@estudante.ifms.edu.br>
2022-09-22 09:36:40 -0400

    card3

--- pages/components/cards.js
+++ pages/components/cards.js
@@ -1,10 +1,10 @@
 export default function Cards(){
     return (
        <div className="cardmargin">
-   <div className="card1"><span className="namebook">Livro Seila OQUE</span></div>
-   <div className="card2"><span className="namebook">Livro Seila OQUE</span></div>
-   <div className="card3"><span className="namebook">Livro Seila OQUE</span></div>
-   <div className="card4"><span className="namebook">Livro Seila OQUE</span></div>
+   <div className="card1"><span className="namebook">HOLOCAUSTO BRASILEIRO</span></div>
+   <div className="card2"><span className="namebook">FORWARD</span></div>
+   <div className="card3"><span className="namebook">O LABIRINTO DO FAUNO</span></div>
+   <div className="card4"><span className="namebook">A PORTA</span></div>
        </div>
     )
 }
--- pages/components/cards2.js
+++ pages/components/cards2.js
@@ -1,10 +1,10 @@
 export default function Cards2(){
     return (
        <div className="cardmargin">
-   <div className="card5"><span className="namebook">Livro Seila OQUE</span></div>
-   <div className="card6"><span className="namebook">Livro Seila OQUE</span></div>
-   <div className="card7"><span className="namebook">Livro Seila OQUE</span></div>
-   <div className="card8"><span className="namebook">Livro Seila OQUE</span></div>
+   <div className="card5"><span className="namebook">CORALINE </span></div>
+   <div className="card6"><span className="namebook">HARRY POTTER E O CÁLICE DE FOGO</span></div>
+   <div className="card7"><span className="namebook">HARRY POTTER E O PRISIONEIRO DE AZKABAN</span></div>
+   <div className="card8"><span className="namebook">TEMPESTADE DE GUERRA</span></div>
        </div>
     )
 }
