b76f4a90cd99c2d9d805c00100bcae463f5b9c19
tainafernanda02 <taina.souza2@estudante.ifms.edu.br>
2022-09-22 08:42:03 -0400

    card

--- pages/components/cards.js
+++ pages/components/cards.js
@@ -3,8 +3,8 @@ export default function Cards(){
        <div className="cardmargin">
    <div className="card1"><span className="namebook">Livro Seila OQUE</span></div>
    <div className="card2"><span className="namebook">Livro Seila OQUE</span></div>
-   <div className="card2"><span className="namebook">Livro Seila OQUE</span></div>
-   <div className="card2"><span className="namebook">Livro Seila OQUE</span></div>
+   <div className="card3"><span className="namebook">Livro Seila OQUE</span></div>
+   <div className="card4"><span className="namebook">Livro Seila OQUE</span></div>
        </div>
     )
 }
---
+++ pages/components/cards2.js
@@ -0,0 +1,10 @@
+export default function Cards(){
+    return (
+       <div className="cardmargin">
+   <div className="card5"><span className="namebook">Livro Seila OQUE</span></div>
+   <div className="card6"><span className="namebook">Livro Seila OQUE</span></div>
+   <div className="card7"><span className="namebook">Livro Seila OQUE</span></div>
+   <div className="card8"><span className="namebook">Livro Seila OQUE</span></div>
+       </div>
+    )
+}
